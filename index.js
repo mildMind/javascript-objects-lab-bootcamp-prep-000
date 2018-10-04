cb6d24d0093e653e394f6efd0f717f6328f1cc34
Spencer D Stone <doomydoomdomm@gmail.com>
2018-10-04 00:30:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var recipes = new Object ({});
 var obj = {prop: 1}
 
 function updateObjectWithKeyAndValue(object, key, value){
+  return obj;
   return Object.assign({}, obj, { [key]: value});
 }
 
