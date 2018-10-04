f2d617ce317d64e36c64d7496a2c99c30fa07bce
Spencer D Stone <doomydoomdomm@gmail.com>
2018-10-04 00:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function deleteFromObjectByKey(object,key)
+function deleteFromObjectByKey(object, key)
+
