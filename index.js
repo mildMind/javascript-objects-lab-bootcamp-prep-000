dfdc79747eaf8b7b3d9788826663d7e72da2a01f
Spencer D Stone <doomydoomdomm@gmail.com>
2018-10-04 00:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var obj = {prop: 1}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, { [key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(key, value){
+  return Object[key] = value
+}
